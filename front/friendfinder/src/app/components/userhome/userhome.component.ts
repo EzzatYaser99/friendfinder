0ad720711770816e6f01ebbd92be25e4e5d83d62
eyasser <eyasser@ntgclarity.com>
2023-11-25 16:00:30 +0200

    edit componant user home

--- front/friendfinder/src/app/components/userhome/userhome.component.ts
+++ front/friendfinder/src/app/components/userhome/userhome.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {PuplishComponent} from "./puplish/puplish.component";
 import {LeftBarComponent} from "./left-bar/left-bar.component";
 import {RightBarComponent} from "./right-bar/right-bar.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {FriendsComponent} from "./friends/friends.component";
+import {CoolImagesComponent} from "./cool-images/cool-images.component";
 
 @Component({
   selector: 'app-userhome',
   standalone: true,
-  imports: [CommonModule, PuplishComponent, LeftBarComponent, RightBarComponent],
+  imports: [CommonModule, PuplishComponent, LeftBarComponent, RightBarComponent, MainPageComponent, FriendsComponent, CoolImagesComponent],
   templateUrl: './userhome.component.html',
   styleUrl: './userhome.component.css'
 })
