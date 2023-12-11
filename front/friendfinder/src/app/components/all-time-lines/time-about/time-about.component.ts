6f16bb81b35ea6fc4187c74243d12bf6202ebbeb
eyasser <eyasser@ntgclarity.com>
2023-12-11 19:48:00 +0200

    refactor in  time lines ( profile - detalis -about -album )

--- front/friendfinder/src/app/components/all-time-lines/time-about/time-about.component.ts
+++ front/friendfinder/src/app/components/all-time-lines/time-about/time-about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TimeLineDetalisComponent} from "../time-line-detalis/time-line-detalis.component";
+import {TimeLinesProfileComponent} from "../time-line-profile/time-lines-profile.component";
 
 @Component({
   selector: 'app-time-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TimeLineDetalisComponent, TimeLinesProfileComponent],
   templateUrl: './time-about.component.html',
   styleUrl: './time-about.component.css'
 })
--- front/friendfinder/src/app/components/all-time-lines/time-lines.component.ts
+++ front/friendfinder/src/app/components/all-time-lines/time-lines.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {TimeLinesProfileComponent} from "./time-line-profile/time-lines-profile.component";
 import {TimeLineDetalisComponent} from "./time-line-detalis/time-line-detalis.component";
 import {TimeLineComponent} from "./time-line/time-line.component";
+import {TimeAboutComponent} from "./time-about/time-about.component";
+import {TimeAlbumComponent} from "./time-album/time-album.component";
 
 @Component({
   selector: 'app-time-lines',
   standalone: true,
-  imports: [CommonModule, TimeLinesProfileComponent, TimeLineDetalisComponent, TimeLineComponent],
+  imports: [CommonModule, TimeLinesProfileComponent, TimeLineDetalisComponent, TimeLineComponent, TimeAboutComponent, TimeAlbumComponent],
   templateUrl: './time-lines.component.html',
   styleUrl: './time-lines.component.css'
 })
